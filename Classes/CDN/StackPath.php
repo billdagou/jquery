0c4dff5b0df2933c1c0c777fc0feea9bdcb2cd35
Bill.Dagou <billdagou@gmail.com>
2020-03-03 11:54:08 +0800

    Remove duplicated directories

--- Classes/CDN/StackPath.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Dagou\Jquery\CDN;
-
-class StackPath extends AbstractCDN {
-    const URL = '//code.jquery.com/';
-}
--- Classes/Cdn/Microsoft.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Dagou\Jquery\CDN;
-
-class Microsoft extends AbstractCDN {
-    const URL = '//ajax.aspnetcdn.com/ajax/jQuery/';
-}
