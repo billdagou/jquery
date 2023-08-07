76b7f18691ae8c227a2cff1fa864af034c74c810
Bill.大狗 <billdagou@gmail.com>
2023-08-07 10:30:27 +0800

    Upgrade for TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'jQuery',
     'description' => 'jQuery, https://jquery.com/',
-    'version' => '3.7.0',
     'category' => 'misc',
+    'author' => 'Bill.Dagou',
+    'author_email' => 'billdagou@gmail.com',
+    'state' => 'stable',
+    'version' => '3.7.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
         ],
     ],
-    'state' => 'stable',
-    'author' => 'Bill.Dagou',
-    'author_email' => 'billdagou@gmail.com',
 ];
